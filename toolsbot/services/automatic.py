52f5dc59c619987015d447b427b6ee1695a2c071
SEVEN-6174 <107546075+SEVEN-6174@users.noreply.github.com>
2025-12-14 19:38:38 +0800

    add msg

--- toolsbot/services/automatic.py
+++ toolsbot/services/automatic.py
@@ -1,6 +1,5 @@
 from toolsbot.configs import DATA_PATH
 
-
 async def init():
     cfg_path = DATA_PATH / "configuration.toml"
     if not cfg_path.exists():
@@ -9,6 +8,7 @@ async def init():
             default_config = f.read()
         with open(cfg_path, "w", encoding="utf-8") as f:
             f.write(default_config)
+        raise RuntimeError("配置文件自动初始化成功...请重新启动bot")
 
 async def shutdown():
     pass
