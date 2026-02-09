041b5e59f658af6f4ecd02a09dd5eb81ca3453b4
BL-BlueLighting <dkotechnology@gmail.com>
2026-02-09 20:20:49 +0800

    feat(likeMe): send like to user. (only qq)

--- plugins/undefiendControllers/__init__.py
+++ plugins/undefiendControllers/__init__.py
@@ -4,7 +4,18 @@ from . import scpfoundation as scp
 from . import whathehell as wht
 from . import ai_funcs as aif
 from . import killers as kle
+from nonebot.adapters.onebot.v11 import *
 
 # UNDEFIEND CONTROLLERS PLUGIN INITALIZATION FILE
 # 这个 __init__.py 并不是主代码文件，而是副代码文件。
 # undefiend 是故意取得名字，没打错。
+
+# 不好归类的东西放 undefiendControllers __init__.py 里头
+
+zanwo = btm.on_command("zanwo", aliases={"赞我", "likeme", "like"}, priority=10)
+
+@zanwo.handle()
+async def _ (bot: Bot, event: GroupMessageEvent | PrivateMessageEvent):
+    uid = event.get_user_id()
+    await bot.call_api("send_like", user_id=uid, times=10)
+    await zanwo.finish("TLoH Bot\n    - 给你点赞啦。记得查收并回赞哦！")
